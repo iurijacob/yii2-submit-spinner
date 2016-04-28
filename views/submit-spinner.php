e62404fb119d18f0871bb9c42854048804925150
Iuri Jacob <iuri@obmep.org.br>
2016-04-28 16:37:18 -0300

    - Multiple forms support

--- views/submit-spinner.php
+++ views/submit-spinner.php
@@ -1,5 +1,8 @@
 <?php
 /** @var $class_id string */
+use yii\helpers\VarDumper;
+use yii\web\View;
+
 /** @var $form_id string */
 /** @var $bg_color string */
 /** @var $bg_opacity float */
@@ -23,82 +26,84 @@ if ($proportionate_increase) {
     $section_offset = $ratio * $section_offset;
 }
 ?>
-<style type="text/css">
+    <style type="text/css">
 
-    <?= '#'. $class_id ?>
-    {
-        display: none
-    ;
-        position: absolute
-    ;
-        left: 0
-    ;
-        top: 0
-    ;
-        width: 100%
-    ;
-        height: 100%
-    ;
-        background-color:
-    <?= $bg_color ?>
-    ;
-        opacity:
-    <?= $bg_opacity ?>
-    ;
-        z-index: 100000
-    ;
-    }
+        <?= '#'. $class_id ?>_<?=$form_id ?>
+        {
+            display: none
+        ;
+            position: absolute
+        ;
+            left: 0
+        ;
+            top: 0
+        ;
+            width: 100%
+        ;
+            height: 100%
+        ;
+            background-color:
+        <?= $bg_color ?>
+        ;
+            opacity:
+        <?= $bg_opacity ?>
+        ;
+            z-index: 100000
+        ;
+        }
 
-    <?= '#'. $class_id ?>
-    .spinner {
-        position: absolute;
-        left: 45%;
-        top: 40%;
-        width: <?= $radius ?>px;
-        height: <?= $radius ?>px;
-        background-color: <?= $bg_spinner_color ?>;
-        border-radius: <?= $radius / 2 ?>px;
-        overflow: hidden;
-    }
+        <?= '#'. $class_id ?>_<?=$form_id ?>
+        .spinner {
+            position: absolute;
+            left: 45%;
+            top: 40%;
+            width: <?= $radius ?>px;
+            height: <?= $radius ?>px;
+            background-color: <?= $bg_spinner_color ?>;
+            border-radius: <?= $radius / 2 ?>px;
+            overflow: hidden;
+        }
 
-    <?= '#'. $class_id ?>
-    .circle {
-        position: absolute;
-        top: <?= $radius / 2 - $section_size / 2 ?>px;
-        left: <?= $radius / 2 - $section_size / 2  ?>px;
-        width: <?= $section_size ?>px;
-        height: <?= $section_size ?>px;
-        border-radius: <?= $section_size / 2 ?>px;
-        background-color: <?= $section_color ?>;
-    }
+        <?= '#'. $class_id ?>_<?=$form_id ?>
+        .circle {
+            position: absolute;
+            top: <?= $radius / 2 - $section_size / 2 ?>px;
+            left: <?= $radius / 2 - $section_size / 2  ?>px;
+            width: <?= $section_size ?>px;
+            height: <?= $section_size ?>px;
+            border-radius: <?= $section_size / 2 ?>px;
+            background-color: <?= $section_color ?>;
+        }
 
-    <?= '#'. $class_id ?>
-    .rotating {
-        -webkit-animation: rotating <?= $spin_speed ?>s linear infinite;
-        -moz-animation: rotating <?= $spin_speed ?>s linear infinite;
-        -ms-animation: rotating <?= $spin_speed ?>s linear infinite;
-        -o-animation: rotating <?= $spin_speed ?>s linear infinite;
-        animation: rotating <?= $spin_speed ?>s linear infinite;
-    }
-</style>
-<div id="<?= $class_id ?>_<?=$form_id ?>">
-    <div class="spinner rotating">
-        <?php for ($i = 0; $i < $sections; $i++) : ?>
-            <div class="circle"
-                 style="transform:  rotate(<?= 360 / $sections * $i ?>deg) translateY(<?= $section_offset ?>px); opacity: <?= ($i / $sections + $section_opacity_base) ?>"></div>
-        <?php endfor ?>
+        <?= '#'. $class_id ?>_<?=$form_id ?>
+        .rotating {
+            -webkit-animation: rotating <?= $spin_speed ?>s linear infinite;
+            -moz-animation: rotating <?= $spin_speed ?>s linear infinite;
+            -ms-animation: rotating <?= $spin_speed ?>s linear infinite;
+            -o-animation: rotating <?= $spin_speed ?>s linear infinite;
+            animation: rotating <?= $spin_speed ?>s linear infinite;
+        }
+    </style>
+    <div id="<?= $class_id ?>_<?=$form_id ?>">
+        <div class="spinner rotating">
+            <?php for ($i = 0; $i < $sections; $i++) : ?>
+                <div class="circle"
+                     style="transform:  rotate(<?= 360 / $sections * $i ?>deg) translateY(<?= $section_offset ?>px); opacity: <?= ($i / $sections + $section_opacity_base) ?>"></div>
+            <?php endfor ?>
+        </div>
     </div>
-</div>
 
-<script type="text/javascript">
+<?
+ob_start();
+?>
     jQuery(function () {
-        set_submit_spinner_form('<?= $form_id ?>');
+    set_submit_spinner_form('<?= $form_id ?>');
     });
     /**
-     *
-     * @param form_id
-     */
-    <?php if ($form_validate == FALSE) : ?>
+    *
+    * @param form_id
+    */
+<?php if ($form_validate == FALSE) : ?>
     function set_submit_spinner_form(form_id) {
         jQuery('#<?= $class_id ?>').height(jQuery(window).height());
         if (form_id.indexOf('#') == -1) {
@@ -113,20 +118,22 @@ if ($proportionate_increase) {
                     jQuery('body').scrollTop(0);
                 });
     }
-    <?php else : ?>
+
+<?php else : ?>
     function set_submit_spinner_form(form_id) {
         jQuery('#<?= $class_id ?>').height(jQuery(window).height());
         if (form_id.indexOf('#') == -1) {
             form_id = '#' + form_id;
         }
         jQuery(form_id)
-            .on('afterValidate', function (event, params) {
-                if (jQuery(form_id).find('.has-error').length == 0) {
-                    show_submit_spinner('<?=$form_id ?>');
-                    jQuery('html').scrollTop(0);
-                    jQuery('body').scrollTop(0);
-                }
-            });
+            .on('afterValidate',
+                function (event, params) {
+                    if (jQuery(form_id).find('.has-error').length == 0) {
+                        show_submit_spinner('<?=$form_id ?>');
+                        jQuery('html').scrollTop(0);
+                        jQuery('body').scrollTop(0);
+                    }
+                });
     }
     <?php endif ?>
 
@@ -137,4 +144,6 @@ if ($proportionate_increase) {
     function hide_submit_spinner(form) {
         jQuery('#<?= $class_id ?>_<?=$form_id ?>').hide();
     }
-</script>
+<?php
+$spinner_js = ob_get_clean();
+$this->registerJs($spinner_js, View::POS_END);
